b0811ce170a7080c072e10cfd587cea37b3abaee
YE GAO <sortsimilar@gmail.com>
2023-07-08 15:51:04 +0800

    Update 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -2,19 +2,20 @@ from transformers import BertConfig, BertModel
 
 # Section 1: Creating a Transformer
 # Building the config
+'''
 config = BertConfig()
 
 # Building the model from the config
 model = BertModel(config)
 
 print(config)
-
+'''
 # Section 2: Different loading methods
 
-
-
-
-
+# Section 3:
+model = BertModel.from_pretrained("bert-base-cased")
+print(model)
+model.save_pretrained("directory_on_my_computer")
 
 
 
