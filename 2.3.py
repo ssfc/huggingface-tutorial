0d95e0cfd07e1ab1d387560175dca0ab75e99084
YE GAO <sortsimilar@gmail.com>
2023-07-08 15:58:56 +0800

    Update 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -1,4 +1,5 @@
 from transformers import BertConfig, BertModel
+import torch
 
 # Section 1: Creating a Transformer
 # Building the config
@@ -18,11 +19,16 @@ print(model)
 model.save_pretrained("directory_on_my_computer")
 
 # Section 4: Using a Transformer model for inference
-
-
-
-
-
+sequences = ["Hello!", "Cool.", "Nice!"]
+encoded_sequences = [
+    [101, 7592, 999, 102],
+    [101, 4658, 1012, 102],
+    [101, 3835, 999, 102],
+]
+model_inputs = torch.tensor(encoded_sequences)
+print("model input:", model_inputs)
+output = model(model_inputs)
+print("model output", output)
 
 
 
