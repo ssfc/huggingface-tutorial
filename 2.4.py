e538c64316bf869cf2d061a1951ad32ea020d7ec
YE GAO <sortsimilar@gmail.com>
2023-07-08 20:06:48 +0800

    Update 2.4.py

--- 2.4.py
+++ 2.4.py
@@ -2,9 +2,10 @@ from transformers import AutoTokenizer
 from transformers import BertTokenizer
 
 # Section 1: Word-based
+'''
 tokenized_text = "Jim Henson was a puppeteer".split()
 print(tokenized_text)
-
+'''
 # Section 2: Character-based
 
 # Section 3: Subword tokenization
@@ -14,14 +15,14 @@ print(tokenized_text)
 # Section 5: Loading and saving
 # tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
 # print(tokenizer)
-
+'''
 tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 print(tokenizer)
 
 result = tokenizer("Using a Transformer network is simple")
 print(result)
 tokenizer.save_pretrained("directory_on_my_computer")
-
+'''
 # Section 6: Encoding
 
 # Section 7: Tokenization
