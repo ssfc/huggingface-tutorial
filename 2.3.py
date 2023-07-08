43fb08ed1ef126f0787653027760cf4a37bf17d3
YE GAO <sortsimilar@gmail.com>
2023-07-08 15:42:45 +0800

    Create 2.3.py

---
+++ 2.3.py
@@ -0,0 +1,22 @@
+from transformers import BertConfig, BertModel
+
+# Section 1: Creating a Transformer
+# Building the config
+config = BertConfig()
+
+# Building the model from the config
+model = BertModel(config)
+
+print(config)
+
+# Section 2: Different loading methods
+
+
+
+
+
+
+
+
+
+
