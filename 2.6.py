fe495356607cb1e94d5f1f8686bf573af2023f1e
YE GAO <sortsimilar@gmail.com>
2023-07-22 13:05:25 +0800

    Update 2.6.py

--- 2.6.py
+++ 2.6.py
@@ -27,5 +27,24 @@ print(model_inputs)
 model_inputs = tokenizer(sequences, padding="max_length", max_length=8)
 print(model_inputs)
 
+# 它还可以截断序列：
+sequences = ["I've been waiting for a HuggingFace course my whole life.", "So have I!"]
+
+# Will truncate the sequences that are longer than the model max length
+# (512 for BERT or DistilBERT)
+model_inputs = tokenizer(sequences, truncation=True)
+print("truncate more than 512: ", model_inputs)
+
+# Will truncate the sequences that are longer than the specified max length
+model_inputs = tokenizer(sequences, max_length=8, truncation=True)
+print("truncate more than 8: ", model_inputs)
+# 这下序列长度还真小于等于8了。
+
+
+
+
+
+
+
 
 
