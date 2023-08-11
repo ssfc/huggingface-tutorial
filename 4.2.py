085bf4fff4d7d34c181214be250be278a1f75621
YE GAO <sortsimilar@gmail.com>
2023-08-11 18:23:15 +0800

    Update 4.2.py

--- 4.2.py
+++ 4.2.py
@@ -2,4 +2,12 @@ from transformers import pipeline
 
 camembert_fill_mask = pipeline("fill-mask", model="camembert-base")
 results = camembert_fill_mask("Le camembert est <mask> :)")
+print(results)
+
+
+
+
+
+
+
 
