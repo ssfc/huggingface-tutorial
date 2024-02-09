5f433786c11767d20043a88953b19503aab924fb
SSFC <sortsimilar@gmail.com>
2024-02-09 23:06:28 +0800

    Update learn_gpt.py

--- learn_gpt.py
+++ learn_gpt.py
@@ -1,12 +1,19 @@
 from transformers import pipeline
+import time
 
 
+start_time = time.time()
 # 模型链接: https://huggingface.co/distilbert/distilgpt2
-# generator = pipeline("text-generation", model="distilgpt2")
+generator = pipeline("text-generation", model="distilgpt2")
+# device default: 22.94s
+# device 0: 21.34s
+# device -1: 22.89s.
+# device cuda: 21.39s.
+
 # generator = pipeline("text-generation", model="gpt2")
 # generator = pipeline("text-generation", model="gpt2-medium")
 # generator = pipeline("text-generation", model="gpt2-large")
-generator = pipeline("text-generation", model="gpt2-xl")
+# generator = pipeline("text-generation", model="gpt2-xl", device=0)
 
 result = generator(
     # "In this course, we will teach you how to",
@@ -19,7 +26,9 @@ result = generator(
 print(result[0])
 print(result[1])
 
-
+end_time = time.time()
+elapsed_time = end_time - start_time
+print("Elapsed Time:", elapsed_time, "seconds")
 
 
 
