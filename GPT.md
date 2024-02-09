419a4d1b1cdd972cf85e2747164337517789e03b
SSFC <sortsimilar@gmail.com>
2024-02-09 11:34:04 +0800

    try gpt2

--- GPT.md
+++ GPT.md
@@ -10,6 +10,8 @@ https://huggingface.co/distilbert/distilgpt2
 
 DistilGPT2 is an English-language model pre-trained with the supervision of the 124 million parameter version of GPT-2. DistilGPT2, which has 82 million parameters. 
 
+DistilGPT2本地存储空间340M。
+
 
 
 Comment:  这样看来distilgpt2并不比gpt2小多少嘛。(2024年2月9日)
--- learn_gpt.py
+++ learn_gpt.py
@@ -2,7 +2,9 @@ from transformers import pipeline
 
 
 # 模型链接: https://huggingface.co/distilbert/distilgpt2
-generator = pipeline("text-generation", model="distilgpt2")
+# generator = pipeline("text-generation", model="distilgpt2")
+generator = pipeline("text-generation", model="gpt2")
+
 result = generator(
     # "In this course, we will teach you how to",
     "I suck a girl's nipples",
