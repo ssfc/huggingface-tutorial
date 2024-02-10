2f664ddb06d4e1fae1bb2898e6e6fff007973d97
SSFC <sortsimilar@gmail.com>
2024-02-10 09:51:52 +0800

    Update learn_gpt.py

--- learn_gpt.py
+++ learn_gpt.py
@@ -5,14 +5,16 @@ import time
 start_time = time.time()
 # 模型链接: https://huggingface.co/distilbert/distilgpt2
 # generator = pipeline("text-generation", model="distilgpt2")
-# device default: 22.94s
+# device 4090 default: 22.94s
+# device 1660s default: 23.46
 # device 0: 21.34s
 # device -1: 22.89s.
 # device cuda: 21.39s.
 # device cpu: 22.88s
 
-# generator = pipeline("text-generation", model="gpt2")
-# device default: 24.30s
+generator = pipeline("text-generation", model="gpt2")
+# device 4090 default: 24.30s
+# device 1660s default: 24.71s
 
 # generator = pipeline("text-generation", model="gpt2-medium")
 # device default: 32.33s
@@ -20,7 +22,7 @@ start_time = time.time()
 # generator = pipeline("text-generation", model="gpt2-large")
 # device default: 45.14s
 
-generator = pipeline("text-generation", model="gpt2-xl")
+# generator = pipeline("text-generation", model="gpt2-xl")
 # device default: 69.23s
 
 result = generator(
