26c2cfcb19c763a62fb07899f0219a548072690f
YE GAO <sortsimilar@gmail.com>
2024-02-10 18:06:48 +0800

    Update 3.2.1.py

--- 3.2.1.py
+++ 3.2.1.py
@@ -5,3 +5,8 @@ from datasets import load_dataset
 raw_datasets = load_dataset("glue", "mrpc")
 print(raw_datasets)
 
+raw_train_dataset = raw_datasets["train"]
+print(raw_train_dataset[0])
+print(raw_train_dataset.features)  # 这是一个鉴别两个句子是否同义的数据集
+
+
