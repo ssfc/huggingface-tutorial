01a27b19e136871b22dd5e93d5968e25e8c6eada
SSFC <sortsimilar@gmail.com>
2024-02-14 00:37:25 +0800

    试试看！查看训练集的元素 15 和验证集的元素 87。他们的标签是什么？

--- 3.2.1.py
+++ 3.2.1.py
@@ -10,4 +10,9 @@ print(raw_train_dataset[0])
 print(raw_train_dataset.features)  # 这是一个鉴别两个句子是否同义的数据集
 
 # ✏️ 试试看！查看训练集的元素 15 和验证集的元素 87。他们的标签是什么？
+print("Element 15 in train set", raw_train_dataset[15])
+
+raw_validation_set = raw_datasets["validation"]
+print("Element 87 in validation set", raw_train_dataset[15])
+
 
