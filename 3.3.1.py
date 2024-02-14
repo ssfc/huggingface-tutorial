8285404d8bc03c71055bcbd6b7d95ed96dc75faa
SSFC <sortsimilar@gmail.com>
2024-02-14 21:24:01 +0800

    Update 3.3.1.py

--- 3.3.1.py
+++ 3.3.1.py
@@ -3,3 +3,7 @@ from transformers import TrainingArguments
 training_args = TrainingArguments("test-trainer")
 
 
+from transformers import AutoModelForSequenceClassification
+checkpoint = "bert-base-uncased"
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+
