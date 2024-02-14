b4f8b269db7f8108c4281e06b41fa784f10ec358
SSFC <sortsimilar@gmail.com>
2024-02-14 18:50:03 +0800

    Create 3.2.3.py

---
+++ 3.2.3.py
@@ -0,0 +1,27 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from transformers import DataCollatorWithPadding
+
+
+def tokenize_function(example):
+    return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+
+raw_datasets = load_dataset("glue", "mrpc")
+print(raw_datasets)
+
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+print("tokenized_datasets:", tokenized_datasets)
+
+
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+samples = tokenized_datasets["train"][:8]
+samples = {k: v for k, v in samples.items() if k not in ["idx", "sentence1", "sentence2"]}
+print([len(x) for x in samples["input_ids"]])
+
+
