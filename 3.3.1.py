c91b892eb5c9204aa4380a754ee4decfa83c5a75
SSFC <sortsimilar@gmail.com>
2024-02-14 22:08:47 +0800

    Update 3.3.1.py

--- 3.3.1.py
+++ 3.3.1.py
@@ -1,9 +1,39 @@
+from datasets import load_dataset
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer
+from transformers import DataCollatorWithPadding
+from transformers import Trainer
 from transformers import TrainingArguments
+
 
 training_args = TrainingArguments("test-trainer")
 
 
-from transformers import AutoModelForSequenceClassification
 checkpoint = "bert-base-uncased"
 model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+def tokenize_function(example):
+    return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+
+raw_datasets = load_dataset("glue", "mrpc")
+print("raw_datasets", raw_datasets)
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+trainer = Trainer(
+    model,
+    training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+)
+
+trainer.train()
+
+
 
