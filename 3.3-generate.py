b9e5f2860da3260567a556a1add1fa7d927c78e2
SSFC <sortsimilar@gmail.com>
2024-02-16 10:13:52 +0800

    Create 3.3-generate.py

---
+++ 3.3-generate.py
@@ -0,0 +1,56 @@
+from datasets import load_dataset
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer
+from transformers import DataCollatorWithPadding
+from transformers import Trainer
+from transformers import TrainingArguments
+import evaluate
+import numpy as np
+
+
+training_args = TrainingArguments("test-trainer")
+
+
+checkpoint = "distilgpt2"
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+def tokenize_function(example):
+    return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+
+raw_datasets = load_dataset("glue", "mrpc")
+print("raw_datasets", raw_datasets)
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+trainer = Trainer(
+    model,  # 选了一个Auto二分model
+    training_args,  # 名字是test-trainer
+    train_dataset=tokenized_datasets["train"],  # glue中的train
+    eval_dataset=tokenized_datasets["validation"],  # glue中的validation
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+)
+
+trainer.train()
+
+predictions = trainer.predict(tokenized_datasets["validation"])
+print(predictions.predictions.shape, predictions.label_ids.shape)
+preds = np.argmax(predictions.predictions, axis=-1)
+
+metric = evaluate.load("glue", "mrpc")
+print(metric.compute(predictions=preds, references=predictions.label_ids))
+
+# 未训练的结果1：{'accuracy': 0.6838235294117647, 'f1': 0.8122270742358079}
+# 未训练的结果2：{'accuracy': 0.5563725490196079, 'f1': 0.6629422718808194}
+# 训练后的结果1：{'accuracy': 0.8627450980392157, 'f1': 0.903448275862069}
+# 训练后的结果2：{'accuracy': 0.8578431372549019, 'f1': 0.901023890784983}
+
+
+
+
+
+
