834304919f3fcf1f08a0a98905f6263d38fcce22
SSFC <sortsimilar@gmail.com>
2024-02-17 13:52:15 +0800

    Update 7.4.2.py

--- 7.4.2.py
+++ 7.4.2.py
@@ -5,6 +5,7 @@ from transformers import DataCollatorForSeq2Seq
 
 model_checkpoint = "Helsinki-NLP/opus-mt-en-fr"
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, return_tensors="pt")
+# 吾人认为，从逻辑上讲先分词再模型, 所以tokenizer应该放在model的前面
 model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint)
 
 data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
