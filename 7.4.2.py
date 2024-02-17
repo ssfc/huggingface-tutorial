4e0f671072339f36a8d41fe12efa6b4254a758f3
SSFC <sortsimilar@gmail.com>
2024-02-17 13:42:55 +0800

    Update 7.4.2.py

--- 7.4.2.py
+++ 7.4.2.py
@@ -1,10 +1,14 @@
 from transformers import AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer
+from transformers import DataCollatorForSeq2Seq
 
+
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, return_tensors="pt")
 
 model_checkpoint = "Helsinki-NLP/opus-mt-en-fr"
 model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint)
 
-
+data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
 
 
 
