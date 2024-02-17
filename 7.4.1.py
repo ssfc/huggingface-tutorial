41b1717dc7892c2896aafb9d4754aefe60cf0fa5
SSFC <sortsimilar@gmail.com>
2024-02-17 11:36:06 +0800

    Update 7.4.1.py

--- 7.4.1.py
+++ 7.4.1.py
@@ -7,8 +7,7 @@ print(raw_datasets)
 split_datasets = raw_datasets["train"].train_test_split(train_size=0.9, seed=20)
 print(split_datasets)
 
-
-
+split_datasets["validation"] = split_datasets.pop("test")
 
 
 
