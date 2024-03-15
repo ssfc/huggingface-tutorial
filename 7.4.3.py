639cb6a66f002fe15d32fb47261a538350b1aa67
SSFC <sortsimilar@gmail.com>
2024-03-15 10:59:21 +0800

    Update 7.4.3.py

--- 7.4.3.py
+++ 7.4.3.py
@@ -5,4 +5,5 @@ model_checkpoint = "marian-finetuned-kde4-en-to-fr/checkpoint-11824"
 translator = pipeline("translation", model=model_checkpoint)
 print(translator("Default to expanded threads"))
 
+print(translator("Unable to import %1 using the OFX importer plugin. This file is not the correct format."))
 
