d56af228fa2355925da5db3f04b3f40c440f1cfa
SSFC <sortsimilar@gmail.com>
2025-06-24 01:09:41 +0800

    def group_texts(examples):

--- 7.3.1.py
+++ 7.3.1.py
@@ -77,3 +77,22 @@ chunks = {
 for chunk in chunks["input_ids"]:
     print(f"'>>> Chunk length: {len(chunk)}'")
 
+def group_texts(examples):
+    # Concatenate all texts
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    # Compute length of concatenated texts
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the last chunk if it's smaller than chunk_size
+    total_length = (total_length // chunk_size) * chunk_size
+    # Split by chunks of max_len
+    result = {
+        k: [t[i : i + chunk_size] for i in range(0, total_length, chunk_size)]
+        for k, t in concatenated_examples.items()
+    }
+    # Create a new labels column
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+lm_datasets = tokenized_datasets.map(group_texts, batched=True)
+print(lm_datasets)
+
