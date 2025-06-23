1e5cd1508225f9633c79e10a44c6a2c3f586b6ef
SSFC <sortsimilar@gmail.com>
2025-06-24 00:46:52 +0800

    from transformers import AutoModelForMaskedLM

---
+++ 7.3.1.py
@@ -0,0 +1,8 @@
+from transformers import AutoModelForMaskedLM
+
+model_checkpoint = "distilbert-base-uncased"
+model = AutoModelForMaskedLM.from_pretrained(model_checkpoint)
+
+distilbert_num_parameters = model.num_parameters() / 1_000_000
+print(f"'>>> DistilBERT number of parameters: {round(distilbert_num_parameters)}M'")
+print(f"'>>> BERT number of parameters: 110M'")
