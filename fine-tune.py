6af06217df6e268b7e7684359e76e687a1641632
SSFC <sortsimilar@gmail.com>
2025-06-28 09:44:53 +0800

    import torch

--- fine-tune.py
+++ fine-tune.py
@@ -31,3 +31,32 @@ trainer = Trainer(
 )
 
 trainer.train()
+
+
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)  # 把模型放到GPU或CPU
+model.eval()
+
+sentence = "This movie is awesome!"
+
+inputs = tokenizer(
+    sentence,
+    return_tensors="pt",
+    truncation=True,
+    padding='max_length',
+    max_length=128
+)
+# 把输入也放到同一设备
+for k in inputs:
+    inputs[k] = inputs[k].to(device)
+
+with torch.no_grad():
+    outputs = model(**inputs)
+    logits = outputs.logits
+    predicted_class_id = logits.argmax(dim=-1).item()
+
+label_map = {0: "negative", 1: "positive"}
+print("预测类别:", predicted_class_id)
+print("情感为:", label_map[predicted_class_id])
