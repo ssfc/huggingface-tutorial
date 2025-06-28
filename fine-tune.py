13bfe0ecb96bd6fe2ee029e2950038ea1b511ce6
SSFC <sortsimilar@gmail.com>
2025-06-28 08:42:06 +0800

    # evaluation_strategy="epoch"

--- fine-tune.py
+++ fine-tune.py
@@ -21,7 +21,7 @@ training_args = TrainingArguments(
     output_dir='./results',
     per_device_train_batch_size=8,
     num_train_epochs=3,
-    evaluation_strategy="epoch"
+    # evaluation_strategy="epoch"
 )
 
 trainer = Trainer(
